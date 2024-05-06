bdb36d50be32cfc94c68f6e038cf60c6629eeb12
HalfS0ur <riveras.kendall99@gmail.com>
2024-05-06 10:25:16 -0600

    deteccion de texto

--- deteccion_texto.py
+++ deteccion_texto.py
@@ -3,5 +3,5 @@ import easyocr
 reader = easyocr.Reader(['es'], gpu=True) #Configura el lector de imagenes (configurar gpu de acuerdo al caso)
 
 def deteccion_de_texto(imagen_placa):
-    texto = reader.readtext(imagen_placa)
-    print(texto)
+    info_deteccion = reader.readtext(imagen_placa)
+    print(info_deteccion)
