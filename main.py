6e17bf519989c6abe1333b4905d32ebe49afd648
HalfS0ur <riveras.kendall99@gmail.com>
2024-05-06 10:04:52 -0600

    Video playback

--- main.py
+++ main.py
@@ -7,5 +7,25 @@ modelo_deteccion_placas = YOLO('modelo_placas_no_final.pt')
 
 #Obtener video
 ruta_video = sys.argv[0]
-video = cv2.VideoCapture(ruta_video)
+video = cv2.VideoCapture('00.mp4')
 
+#Procesar video
+while True: #Ciclo infinito
+    _, cuadro = video.read() #Abre el video cuadro por cuadro
+    detecciones = modelo_deteccion_placas(cuadro, conf=0.61) #Detecta placas con un intervalo de confianza mayor a 0.61%
+
+    cajas = detecciones[0].boxes #Cajas que encierran las placas en el video
+    for caja in cajas:
+        x1, y1, x2, y2 = caja.xyxy.squeeze().tolist() #Coordenadas de la caja de detección
+        confianza = float(caja.conf) #Valor de confianza de la placa identificada
+
+        if confianza <= 0.61:
+            imagen_placa = cuadro[int(y1):int(y2), int(x1):int(x2), :] #Recorta la placa del cuadro completo
+            escala_grises = cv2.cvtColor(imagen_placa, cv2.COLOR_BGR2GRAY) #Convierte la imagen de la placa a escala de grises
+            threshold_img = cv2.threshold(escala_grises, 64, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1] #Aplica filtros para eliminar el ruido
+
+    cv2.imshow('Video', cuadro)
+    if (cv2.waitKey(20) == ord('q')):
+        break
+
+cv2.destroyAllWindows()
