aec6211235c0761c1ce7a2e158f4663652587aa6
HalfS0ur <riveras.kendall99@gmail.com>
2024-05-06 10:18:19 -0600

    prueba

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ modelo_deteccion_placas = YOLO('modelo_placas_no_final.pt')
 
 #Obtener video
 ruta_video = str(sys.argv[1])
-print(ruta_video)
 video = cv2.VideoCapture(ruta_video)
 
 #Procesar video
@@ -20,10 +19,14 @@ while True: #Ciclo infinito
         x1, y1, x2, y2 = caja.xyxy.squeeze().tolist() #Coordenadas de la caja de detección
         confianza = float(caja.conf) #Valor de confianza de la placa identificada
 
-        if confianza <= 0.61:
+        if confianza != -1:
             imagen_placa = cuadro[int(y1):int(y2), int(x1):int(x2), :] #Recorta la placa del cuadro completo
             escala_grises = cv2.cvtColor(imagen_placa, cv2.COLOR_BGR2GRAY) #Convierte la imagen de la placa a escala de grises
             threshold_img = cv2.threshold(escala_grises, 64, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1] #Aplica filtros para eliminar el ruido
+
+            cv2.imshow('Placa procesada', threshold_img) #Salida de prueba
+
+    #cuadro_ = detecciones[0].plot()
 
     cv2.imshow('Video', cuadro)
     if (cv2.waitKey(20) == ord('q')):
