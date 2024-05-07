f367e7715e5a1cca6d1cc36ce1b85693906e9ea8
HalfS0ur <riveras.kendall99@gmail.com>
2024-05-07 15:38:48 -0600

    Manejo de deteccion_texto vacío

--- deteccion_texto.py
+++ deteccion_texto.py
@@ -5,10 +5,10 @@ reader = easyocr.Reader(['es'], gpu=True) #Configura el lector de imagenes (conf
 def deteccion_de_texto(imagen_placa):
     numero_placa = None
     info_deteccion = reader.readtext(imagen_placa)
-    
-    if info_deteccion[0][2] >= 0.9:
-        numero_placa = info_deteccion[0][1]
-        return numero_placa
+    if info_deteccion:
+        if info_deteccion[0][2] >= 0.9:
+            numero_placa = info_deteccion[0][1]
+            return numero_placa
 
     else:
         return numero_placa
