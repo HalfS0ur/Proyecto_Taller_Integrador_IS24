e81707abb08e93423fb60af66ffe50b5e982adb7
HalfS0ur <riveras.kendall99@gmail.com>
2024-05-08 08:37:21 -0600

    Mejora del jitter

--- main.py
+++ main.py
@@ -28,11 +28,12 @@ while True: #Ciclo infinito
         if confianza != -1:
             imagen_placa = cuadro[int(y1):int(y2), int(x1):int(x2), :] #Recorta la placa del cuadro completo
             escala_grises = cv2.cvtColor(imagen_placa, cv2.COLOR_BGR2GRAY) #Convierte la imagen de la placa a escala de grises
-            threshold_img = cv2.threshold(escala_grises, 64, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1] #Aplica filtros para eliminar el ruido
+            threshold_img = cv2.threshold(escala_grises, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1] #Aplica filtros para eliminar el ruido
             
             cv2.imshow('Placa', threshold_img) #Salida de prueba
 
             numero_placa = deteccion_de_texto(threshold_img)
+            print(numero_placa) #Salida de prueba
 
             if numero_placa is not None:
                 cuadro_numero_placa = cv2.putText(cuadro, numero_placa, (int(x1), int(y1)), cv2.FONT_HERSHEY_SIMPLEX, 6, (128, 17, 0), 7, cv2.LINE_AA)
@@ -41,13 +42,17 @@ while True: #Ciclo infinito
             else:
                 if numero_placa_prev is not None:
                     cuadro_numero_placa = cv2.putText(cuadro, numero_placa_prev, (int(x1), int(y1)), cv2.FONT_HERSHEY_SIMPLEX, 6, (128, 17, 0), 7, cv2.LINE_AA)
-            
-            print(numero_placa) #Salida de prueba
-           
+
+    if numero_placa_prev is not None:
+            cuadro_numero_placa = cv2.putText(cuadro, numero_placa_prev, (int(x1), int(y1)), cv2.FONT_HERSHEY_SIMPLEX, 6, (128, 17, 0), 7, cv2.LINE_AA)
+
+    if not cajas:
+            numero_placa_prev = None
+                     
     #cuadro_ = detecciones[0].plot()
 
     cv2.imshow('Video', cuadro_numero_placa)
-    if (cv2.waitKey(50) == ord('q')):
+    if (cv2.waitKey(30) == ord('q')):
         break
 
 cv2.destroyAllWindows()
