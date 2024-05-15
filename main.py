3a670ac13c1b24c6fd43ba5d6ee2ecc3c27579ee
HalfS0ur <riveras.kendall99@gmail.com>
2024-05-14 21:53:39 -0600

    idk

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ video = cv2.VideoCapture(ruta_video)
 #Inicializar variable para guardar el número de placa previo
 numero_placa_prev = None
 
+dict_placas = {}
+dict_coords = {}
+
 #Procesar video
 while True: #Ciclo infinito
     _, cuadro = video.read() #Abre el video cuadro por cuadro
@@ -21,6 +24,7 @@ while True: #Ciclo infinito
     cuadro_ = cv2.resize(cuadro, (1920, 1080))
 
     detecciones = modelo_deteccion_placas.track(cuadro_, conf=0.61) #Detecta placas con un intervalo de confianza mayor a 0.61%
+
     cuadro_numero_placa = cuadro
 
     cajas = detecciones[0].boxes #Cajas que encierran las placas en el video
@@ -28,37 +32,44 @@ while True: #Ciclo infinito
         x1, y1, x2, y2 = caja.xyxy.squeeze().tolist() #Coordenadas de la caja de detección
         confianza = float(caja.conf) #Valor de confianza de la placa identificada
         id = int(caja.id)
-        aver = str(id)
-        print(id)
+        dict_coords[id] = int(x1), int(y1)
 
-        if id != -1:
+        if id != -1:   
             imagen_placa = cuadro[int(y1):int(y2), int(x1):int(x2), :] #Recorta la placa del cuadro completo
             escala_grises = cv2.cvtColor(imagen_placa, cv2.COLOR_BGR2GRAY) #Convierte la imagen de la placa a escala de grises
             threshold_img = cv2.threshold(escala_grises, 128, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1] #Aplica filtros para eliminar el ruido
+            erosion_img = cv2.erode(threshold_img, (15,15), iterations=3)
             
-            cv2.imshow('Placa', threshold_img) #Salida de prueba
+            cv2.imshow('Placa', erosion_img) #Salida de prueba
 
             numero_placa = deteccion_de_texto(threshold_img)
             print(numero_placa) #Salida de prueba
+            
 
             if numero_placa is not None:
-                cuadro_numero_placa = cv2.putText(cuadro, aver, (int(x1), int(y1)), cv2.FONT_HERSHEY_SIMPLEX, 6, (128, 17, 0), 7, cv2.LINE_AA)
-                numero_placa_prev = aver
+                test = numero_placa
+                #cuadro_numero_placa = cv2.putText(cuadro, test, (int(x1), int(y1)), cv2.FONT_HERSHEY_SIMPLEX, 6, (128, 17, 0), 7, cv2.LINE_AA)
+                numero_placa_prev = test
+                dict_placas[id] = test
+                
 
-            else:
-                if numero_placa_prev is not None:
-                    cuadro_numero_placa = cv2.putText(cuadro, numero_placa_prev, (int(x1), int(y1)), cv2.FONT_HERSHEY_SIMPLEX, 6, (128, 17, 0), 7, cv2.LINE_AA)
+            #else:
+                #if numero_placa_prev is not None:
+                    #cuadro_numero_placa = cv2.putText(cuadro, test, (int(x1), int(y1)), cv2.FONT_HERSHEY_SIMPLEX, 6, (128, 17, 0), 7, cv2.LINE_AA)
 
-    if numero_placa_prev is not None:
-            cuadro_numero_placa = cv2.putText(cuadro, numero_placa_prev, (int(x1), int(y1)), cv2.FONT_HERSHEY_SIMPLEX, 6, (128, 17, 0), 7, cv2.LINE_AA)
+    #if numero_placa_prev is not None:
+            #cuadro_numero_placa = cv2.putText(cuadro, numero_placa_prev, (int(x1), int(y1)), cv2.FONT_HERSHEY_SIMPLEX, 6, (128, 17, 0), 7, cv2.LINE_AA)
 
-    if not cajas:
-            numero_placa_prev = None
-                     
-    cuadro_ = detecciones[0].plot()
+    #if not cajas:
+            #numero_placa_prev = None
+
+    for key in dict_placas:
+        cuadro_numero_placa = cv2.putText(cuadro, dict_placas[key], dict_coords[key], cv2.FONT_HERSHEY_SIMPLEX, 6, (128, 17, 0), 7, cv2.LINE_AA)
+
+    print(dict_placas)
 
     cuadro_ajustado = cv2.resize(cuadro_numero_placa, (1080, 720))
-    cv2.imshow('Video', cuadro_)
+    cv2.imshow('Video', cuadro_ajustado)
     if (cv2.waitKey(1) == ord('q')):
         break
 
